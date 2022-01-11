d5024b9b182c3bdd2eada7f633c49be9bffde5a4
Nick Minkler (Sleaker) <sleaker@gmail.com>
2022-01-10 16:20:00 -0800

    fix hacks decrementing number of grows causing hacks to scale unbound.
    Add clarity in printed logs.
    Add design overview for current main loop

--- control.js
+++ control.js
@@ -4,6 +4,7 @@
 
 import { findAllServers } from "./util.js";
 import { Zombie, newZombie } from './zombie.js';
+import * as Util from './util.js';
 import * as logger from "./log.js";
 
 /** 
@@ -92,6 +93,41 @@ function getRunningScriptCounts(servers, target) {
 }
 
 /**
+ * Current algorithm:
+ * -> count total server capacity of available threads amongst all rooted servers
+ * -> Split total number of threads into ratio of grow, hack, and weaken (.84, .04, .12)
+ * -> for each server
+ * 		-> Count number of Grow, Hack, or Weaken already running 
+ * 		-> update number of available threads for the server
+ * -> For each server
+ * 		-> if grow capacity is not filled start growing up to grow capacity or max threads available on server
+ * 			-> decrement total grow capacity
+ * 			-> decrement number of available threads on this server
+ * 		-> if hack capacity is not filled start hacking up to hack capacity or max threads available on server
+ * 			-> decrement total hack capacity
+ * 			-> decrement number of available threads on this server
+ * 		-> if grow or hack is already running on the server or capacity for both is filled, start weakening
+ * 
+ * Current throughput: ~2150 threads targetting 'phantasy' results in ~$2.3 mil/sec and ~160 xp/sec 
+ * 
+ * TODO: Instead of splitting capacity across all servers based on simple ratios we should create a 'cycle'
+ *       And determine the optimal number of cycles that can be configured against a given target. Each cycle will need to be offset
+ *       So it doesn't conflict with another servers cycle using the same target.
+ * 
+ * -> Start weaken 1 (60 sec base runtime)
+ * -> pause 1 for delay between hack/grow completion
+ * -> start weaken 2 (60 sec base runtime)
+ * -> pause 2 for slightly less than 15 seconds * time multiplier (allow to grow to complete before)
+ * -> start grow (45 sec base runtime)
+ * -> pause 3 for ~30 seconds * time multiplier
+ * -> start hack (15 sec base runtime)
+ * -> pause 4 until:
+ * 	 -> hack completes
+ *   -> weaken 1 completes
+ *   -> grow completes
+ *   -> weaken 2 completes
+ * -> restart cycle
+ * 
  * @param {NS} ns
  * @param {Zombie[]} servers
  * @param {Zombie} target
@@ -106,9 +142,10 @@ async function hackTarget(ns, servers, target, setup = true) {
 
 		// ns.print("Running | Grow: " + runningGrow + " | Hack: " + runningHack + " | Weaken: " + runningWeaken);
 		logger.debug("Running | Grow: %(grow)s | Hack: %(hack)s | Weaken: %(weak)s", { grow: runningGrow, hack: runningHack, weak: runningWeaken });
-		const growRate = target.shouldGrow ? (setup ? .9 : .84) : 0;
+		const growRate =  setup ? target.shouldGrow ? .9 : 0 : .84;
+		
 		let growThreads = Math.floor(maxThreads * growRate) - runningGrow;
-		const hackRate = setup ? 0 : .09;
+		const hackRate = setup ? 0 : .04;
 		let hackThreads = Math.floor(maxThreads * hackRate) - runningHack;
 		// ns.print("Wanted grow threads: " + growThreads);
 		for (const zombie of servers) {
@@ -126,7 +163,7 @@ async function hackTarget(ns, servers, target, setup = true) {
 				ns.exec("grow.js", zombie.hostname, toGrow, target.hostname);
 				
 				await ns.sleep(5);
-				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "grow.js")[0]);
+				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "grow.js")[0] + " -> Money: " + Util.formatMoney(target.availableMoney) + " / " + Util.formatMoney(target.maxMoney));
 				availableRunners -= toGrow;
 				growThreads -= toGrow;
 			}
@@ -139,9 +176,9 @@ async function hackTarget(ns, servers, target, setup = true) {
 				ns.exec("hack.js", zombie.hostname, toHack, target.hostname);
 				
 				await ns.sleep(5);
-				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "grow.js")[0]);
+				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "hack.js")[0] + " <- Money " + Util.formatMoney(target.availableMoney));
 				availableRunners -= toHack;
-				growThreads -= toHack;
+				hackThreads -= toHack;
 			}
 	
 			// TODO: handle additional weaken scripts better
@@ -150,7 +187,7 @@ async function hackTarget(ns, servers, target, setup = true) {
 				ns.exec("weaken.js", zombie.hostname, availableRunners, target.hostname);
 
 				await ns.sleep(5);
-				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "weaken.js")[0]);
+				ns.print(zombie.hostname + " " + zombie.getRunningScriptLogs(target, "weaken.js")[0] + " -> " + target.currentSecurity);
 			}
 		}
 
